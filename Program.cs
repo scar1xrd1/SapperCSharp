ace4a091a33cf61b779e85beceb59d111a91744c
Anton Kotlyar <kotlaranton551@gmail.com>
2023-04-09 16:23:19 +0300

    добавлена рекурсивная функция

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ interface ISapper
     void GenerateMines(int quantity);
     void GenerateNums();
     //void InputHandler(string key);
+    void CheckEmptyCells(int y, int x, bool IsNum);
 
     void PrintField();
     int CheckBox(int x, int y);
@@ -28,7 +29,8 @@ class Dot
 {
     public int Type { get; set; } // -2(#) - флаг / -1(*) - мина / 0(.) - пустая клетка / 123456789 цифры
     public bool Show { get; set; }
-    public Dot() { Type = 0; }
+    public bool Checked { get; set; }
+    public Dot() { Type = 0; Checked = false; }
 }
 
 class Sapper : ISapper
@@ -36,7 +38,7 @@ class Sapper : ISapper
     Random rnd = new Random();
 
     int y, x, mines = 0, flags = 0;
-    int[] mousePTR = {0, 0};
+    int[] mousePTR;
     Dot[,] field; // i = y, j = x 
 
     public Sapper(int y, int x)
@@ -45,6 +47,8 @@ class Sapper : ISapper
         this.y = y;
         this.x = x;
 
+        mousePTR = new int[2]{ y / 2, x / 2 };
+
         field = new Dot[y, x];
 
         for (int i = 0; i < y; i++)
@@ -52,31 +56,62 @@ class Sapper : ISapper
                 field[i, j] = new Dot();
 
         if(y*x > 50) GenerateMines(rnd.Next(7, 13));
-        if(y*x > 75) GenerateMines(rnd.Next(14, 23));
-        if (y * x > 150) GenerateMines(rnd.Next(24, 30));
-        else GenerateMines(rnd.Next(4, 14));
+        else if(y*x > 75) GenerateMines(rnd.Next(10, 15));
+        else if (y * x > 150) GenerateMines(rnd.Next(15, 25));
+        else GenerateMines(rnd.Next(4, 9));
 
         GenerateNums();
     }
 
     public void Start()
     {
+        bool lose = false;
+        bool win = false;
+
         while(true)
         {
+            if(lose)
+            {
+                Console.WriteLine("Вы взорвались!!!");
+                break;
+            }
+
             PrintField();
             Console.WriteLine($"Осталось мин: {mines - flags}\n");
             Console.WriteLine("Управление:\nСтрелочки на клавиатуре - двигать курсор\nENTER - Поставить флаг\nSPACE - Вскрыть клетку\nBACKSPACE - Убрать флаг"); ;
 
-            Console.WriteLine(y + " " + x);
-            Console.WriteLine(mousePTR[0] + " " + mousePTR[1]);
-
             ConsoleKeyInfo ch = Console.ReadKey(true);
             int code = ch.GetHashCode();
 
-            if (Key(code) == "right") { if (mousePTR[1] < x-1) mousePTR[1]++; }
-            else if (Key(code) == "left") { if (mousePTR[1] > 0) mousePTR[1]--; }
-            else if (Key(code) == "up") { if (mousePTR[0] > 0) mousePTR[0]--; }
-            else if (Key(code) == "down") { if (mousePTR[0] < y-1) mousePTR[0]++; }
+            if (Key(code) == "right") { if (mousePTR[1] < x-1) mousePTR[1]++; Console.Beep(200, 100); }
+            else if (Key(code) == "left") { if (mousePTR[1] > 0) mousePTR[1]--; Console.Beep(250, 100); }
+            else if (Key(code) == "up") { if (mousePTR[0] > 0) mousePTR[0]--; Console.Beep(300, 100); }
+            else if (Key(code) == "down") { if (mousePTR[0] < y-1) mousePTR[0]++; Console.Beep(350, 100); }
+            else if(Key(code) == "space")
+            {
+                if (field[mousePTR[0], mousePTR[1]].Type == -1)
+                {
+                    lose = true;
+                    Console.Beep(450, 250);
+                    Console.Beep(250, 250);
+                    Console.Beep(200, 250);
+                }
+                else if (field[mousePTR[0], mousePTR[1]].Type > 0 && !field[mousePTR[0], mousePTR[1]].Show)
+                {
+                    Console.Beep(450, 250);
+                    Console.Beep(550, 50);
+                    Console.Beep(650, 50);
+                    field[mousePTR[0], mousePTR[1]].Show = true;
+                }
+                else
+                {
+                    Console.Beep(650, 250);
+                    Console.Beep(750, 50);
+                    Console.Beep(850, 50);
+                    CheckEmptyCells(mousePTR[0], mousePTR[1], false);
+                }
+            }
+            
             Console.Clear();
         }        
     }
@@ -105,6 +140,102 @@ class Sapper : ISapper
 
         return "";
     }   
+
+    public void CheckEmptyCells(int y, int x, bool IsNum)
+    {
+        if (field[y, x].Checked) return;
+
+        if (CheckBox(x, y) == 0 || field[y, x].Type > 0 && field[y, x].Show == false)
+        {
+            field[y, x].Show = true;
+            field[y, x].Checked = true;
+
+            if (x + 1 < this.x)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y, x + 1].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y, x + 1, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y, x + 1, false);
+                }                
+            }
+            if (x - 1 >= 0)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y, x - 1].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y, x - 1, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y, x - 1, false);
+                }                
+            }
+            
+            if (x - 1 >= 0 && y - 1 >= 0)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y - 1, x - 1].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y - 1, x - 1, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y - 1, x - 1, false);
+                }                
+            }
+            if (x - 1 >= 0 && y + 1 < this.y)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y + 1, x - 1].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y + 1, x - 1, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y + 1, x - 1, false);
+                }                
+            }
+
+            if (y + 1 < this.y)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y + 1, x].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y + 1, x, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y + 1, x, false);
+                }                
+            }
+            if (y - 1 >= 0)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y - 1, x].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y - 1, x, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y - 1, x, false);
+                }                
+            }
+            
+            if (y - 1 >= 0 && x - 1 >= 0)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y - 1, x - 1].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y - 1, x - 1, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y - 1, x - 1, false);
+                }                
+            }
+            if (y - 1 >= 0 && x + 1 < this.x)
+            {
+                if (field[y, x].Type > 0) return;
+                if (field[y - 1, x + 1].Type >= 0 && !IsNum)
+                {
+                    if(field[y, x].Type > 0) CheckEmptyCells(y - 1, x + 1, true);
+                    else if (field[y, x].Type == 0) CheckEmptyCells(y - 1, x + 1, false);
+                }                
+            }
+            //if (x - 1 > 0)
+            //{
+            //    if (field[y, x].Type > 0) return;
+            //    if (field[y, x - 1].Type >= 0 && !IsNum)
+            //    {
+            //        if(field[y, x].Type > 0) CheckEmptyCells(y, x - 1, true);
+            //        else if (field[y, x].Type == 0) CheckEmptyCells(y, x - 1, false);
+            //    }                
+            //}
+        }
+    }
 
     public int CheckBox(int x, int y)
     {
